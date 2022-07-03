e1c65e7213f67de22bd0d4cd3553190caf846a6e
artyom-voitas <artyom.voitas@gmail.com>
2022-07-03 21:09:45 +0300

    feat(clients): created table with clients info

--- src/js/clients.js
+++ src/js/clients.js
@@ -1,6 +1,7 @@
 import {
     logout,
     unauthorizedUserCheck,
+    formatDate
 } from './utils.js'
 logout()
 
@@ -23,7 +24,50 @@ async function fetchUsers() {
 
 fetchUsers().then((users) => {
     console.log('Users', users)
+    let tableBody = document.querySelector('tbody');
+    let maleUsersCounter = 0;
+    let femaleUsersCounter = 0;
+    for (const user of users) {
+        let tableRow = document.createElement('tr');
+        tableRow.classList.add('table-row');
+        tableBody.append(tableRow)
+        let userName = document.createElement('td');
+        let userCompany = document.createElement('td');
+        let userEmail = document.createElement('td');
+        let userPhoneNumber = document.createElement('td');
+        let userBalance = document.createElement('td');
+        let userRegistrationDate = document.createElement('td');
+        let userDate = new Date((user['registered']).substr(0, 10));
+        userName.innerHTML = user['name'];
+        userCompany.innerHTML = user['company'];
+        userEmail.innerHTML = user['email'];
+        userPhoneNumber.innerHTML = user['phone'];
+        userBalance.innerHTML = user['balance'];
+        userRegistrationDate.innerHTML = formatDate(userDate);
+        tableRow.append(userName);
+        tableRow.append(userCompany);
+        tableRow.append(userEmail);
+        tableRow.append(userPhoneNumber);
+        tableRow.append(userBalance);
+        tableRow.append(userRegistrationDate);
+        if(!user['isActive']) {
+            tableRow.style.background = 'gray';
+        }
+        if(user['gender'] === 'male') {
+            maleUsersCounter += 1;
+        } else if (user['gender'] === 'female') {
+            femaleUsersCounter += 1;
+        }
+    }
+    let maleUsers = document.querySelector('.male__users');
+    maleUsers.innerHTML = String(maleUsersCounter);
+    let femaleUsers = document.querySelector('.female__users');
+    femaleUsers.innerHTML = String(femaleUsersCounter);
+
 })
+
+
+
 
 unauthorizedUserCheck()
 
